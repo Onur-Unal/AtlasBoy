859b1969179945bff706c46309726fbd575305f9
Onur Ünal <spaceonur@gmail.com>
2025-04-26 00:15:00 +0300

    fix

--- app/layout.js
+++ app/layout.js
@@ -1,34 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+"use client";
+
 import "./globals.css";
-import Head from "next/head";
 import { basePath } from "@/lib/config";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Atlas Boy Birthday Party!",
-  description: "This little man is turning 1!",
-};
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+        <title>Atlas 1 Yaşında!</title>
         <link rel="icon" href={`${basePath}/favicon.ico`} />
       </Head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
